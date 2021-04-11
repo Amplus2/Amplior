587e628d3120387d279074812773cdb7f12e4847
Luddifee <ludwig@ludwig-lehnert.de>
2021-04-11 20:45:06 +0200

    starting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Amplior/main.dart';
+import 'package:amplior/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
